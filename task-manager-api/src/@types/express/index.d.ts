6824f144d887c7db647defcfd30134f409e81204
Kuldeep Verma <>
2026-04-03 16:23:27 +0530

    Keep only frontend code

--- task-manager-api/src/@types/express/index.d.ts
+++
@@ -1,9 +0,0 @@
-import 'express';
-
-declare module 'express-serve-static-core' {
-  interface Request {
-    user?: {
-      userId: string;
-    };
-  }
-}
--- task-manager-api/src/config/jwt.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-
-export const JWT_CONFIG = {
-  SECRET: process.env.JWT_SECRET || 'strong_access_secret_0987654321',
-  REFRESH_SECRET: process.env.REFRESH_TOKEN_SECRET || 'strong_refresh_secret_1234567890',
-  EXPIRE: process.env.JWT_EXPIRE || '15m',
-  REFRESH_EXPIRE: process.env.REFRESH_TOKEN_EXPIRE || '7d'
-};
